4bafdb304201a6383cbd382f145119c58beda7cf
JuanP539 <jpbarriosgarcia@gmail.com>
2026-06-05 20:24:58 -0300

    Extra signer added

--- Entrega_2/README.md
+++ Entrega_2/README.md
@@ -149,6 +149,7 @@ La app estará disponible en `http://localhost:5173`.
 | 1 | `0x447b46E7a4C959fE30eBCdF01eA2B83eDFC2FC8a` |
 | 2 | `0x1066594e4483AE78eb21ECE5F475f8f9b5c8224d` |
 | 3 | `0x5478eEE9fbe0c2a994395A1848c62583F376fa2F` |
+| 4 | `0x0f1925e7003fbF4b4315409A3c78e65bd1F4B4dB` |
 
 ---
 
--- Entrega_2/scripts/deploy.ts
+++ Entrega_2/scripts/deploy.ts
@@ -10,6 +10,7 @@ async function main() {
     '0x447b46E7a4C959fE30eBCdF01eA2B83eDFC2FC8a', // signer 1
     '0x1066594e4483AE78eb21ECE5F475f8f9b5c8224d', // signer 2
     '0x5478eEE9fbe0c2a994395A1848c62583F376fa2F', // signer 3
+    '0x0f1925e7003fbF4b4315409A3c78e65bd1F4B4dB', // signer 4
   ];
   const threshold = 2; // requires 2-of-3 approvals
   // ───────────────────────────────────────────────────────────────────────────
